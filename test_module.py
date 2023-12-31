ddeedee5f291e524cf63342ab832d7c07b0ca612
Евгений Мазур <Doctor-ts2017@yandex.ru>
2023-12-31 09:04:58 +0300

    code according to PEP8

--- test_module.py
+++ test_module.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
-files_list = ['main.py', 'readme.md']
+files_list = ['main.py', 'readme.md'
 
 
 def test_program():
